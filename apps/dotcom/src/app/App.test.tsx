a0c109d07d5d0a15430b5aac0214cec2f7e0386b
Ryan Jung <ryan.matthew.jung@gmail.com>
2023-04-24 13:37:05 -0400

    🧪 test: reintroduce test

--- apps/dotcom/src/app/App.test.tsx
+++ apps/dotcom/src/app/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+// i.e. `.toBeVisible`
+import '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
-  /* const { getByText } = render(<App />);
-   * const name = ('Code4Community');
-   * expect(name).toBeInTheDocument(); */
+test('renders learn react link', async () => {
+  const { findByText } = render(<App />);
+  expect(await findByText(/software solutions/i)).toBeVisible();
 });
