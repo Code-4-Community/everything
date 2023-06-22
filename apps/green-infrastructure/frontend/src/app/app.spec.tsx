90aa75228f6a5d63ba7f6a08e1b6d398b69a70ca
Kenny Jung <dukyoung01@naver.com>
2023-06-21 21:10:47 -0400

    Migrate to nx 16 and add green infrastructure

---
+++ apps/green-infrastructure/frontend/src/app/app.spec.tsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+
+import { BrowserRouter } from 'react-router-dom';
+
+describe('App', () => {
+  it('should pass', () => {
+    expect(1 + 1).toBe(2);
+  });
+  /* it('should render successfully', () => {
+   *   const { baseElement } = render(
+   *     <BrowserRouter>
+   *       <App />
+   *     </BrowserRouter>
+   *   );
+   *   expect(baseElement).toBeTruthy();
+   * });
+
+   * it('should have a greeting as the title', () => {
+   *   const { getByText } = render(
+   *     <BrowserRouter>
+   *       <App />
+   *     </BrowserRouter>
+   *   );
+   *   expect(getByText(/Welcome frontend/gi)).toBeTruthy();
+   * }); */
+});
