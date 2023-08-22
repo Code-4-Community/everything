11fbd6d3f87395ce790c935c880eab31773af38a
Hamsini Malli Sivakumar <hmallisi@broadinstitute.org>
2023-08-21 23:25:36 -0400

    WIP

--- apps/green-infrastructure/frontend/src/main.tsx
+++ apps/green-infrastructure/frontend/src/main.tsx
@@ -1,16 +1,20 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './app/app';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
