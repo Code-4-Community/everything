3800c4bc6b12299e6ab663aeb11bf4655b407cd7
Hamsini Malli Sivakumar <hmallisi@broadinstitute.org>
2023-08-24 21:21:01 -0400

    added gi locations for markers

--- apps/green-infrastructure/frontend/src/app/app.tsx
+++ apps/green-infrastructure/frontend/src/app/app.tsx
@@ -1,12 +1,11 @@
-import MapWithPopup from '../components/map/SampleMap';
+import Map from '../components/map/SampleMap';
 
 const App: React.FC = () => {
   const mapCenter: google.maps.LatLngLiteral = { lat: 42.36, lng: -71.06 };
  
   return (
     <div>
-      <h1>Green Infrastructure!</h1>
-      <MapWithPopup zoom={8} center={mapCenter} />
+      <Map zoom={8} center={mapCenter} />
     </div>
   );
 };
