78f34f1b27a29f48198a090b3416175d62c5ad05
mahekagg <105991553+mahekagg@users.noreply.github.com>
2023-08-31 14:12:20 -0400

    added checkboxes and site type icons

--- apps/green-infrastructure/frontend/src/components/map/MapLegend.tsx
+++ apps/green-infrastructure/frontend/src/components/map/MapLegend.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import { Button, Typography, Image } from 'antd';
+import { Button, Typography, Image, Checkbox } from 'antd';
 import { useState } from 'react';
-import { SITE_OPTIONS_ROADMAP } from '../../constants';
+import { SITE_STATUS_ROADMAP, SITE_TYPE_ROADMAP } from '../../constants';
 
 
 const MapLegendContainer = styled.div`
@@ -12,16 +12,27 @@ const MapLegendContainer = styled.div`
   gap: 20px;
 `;
 
+const CheckboxContainer = styled.div`
+  display: flex;
+  align-items: right;
+  gap: 8px;
+`;
+
+const CheckboxLabel = styled.label`
+  margin-bottom: 0;
+`;
+
 const LegendItem = styled.div`
   width: 100%;
   display: flex;
   gap: 0 40px;
+  align-items: center;
 `;
 
 const LegendImage = styled(Image)`
   height: 20px;
   width: 20px;
-  display: inline-block;
+  float: left;
 `;
 
 // const ToggleTextButton = styled(Button)`
@@ -41,22 +52,56 @@ const MapLegend: React.FC<MapLegendProps> = ({ canHide, icons }) => {
     //     setShowLegend((prevState) => !prevState);
     //   };
 
+    const [selectedCheckboxes, setSelectedCheckboxes] = useState<string[]>([]);
+
+    const handleCheckboxChange = (icon: string) => {
+        setSelectedCheckboxes(prevSelected => {
+          if (prevSelected.includes(icon)) {
+            return prevSelected.filter(selected => selected !== icon);
+          } else {
+            return [...prevSelected, icon];
+          }
+        });
+      };
+
+
     const [availableIcon, adoptedIcon, futureIcon] =
-      icons ?? SITE_OPTIONS_ROADMAP.map((option) => option.image);
+      icons ?? SITE_STATUS_ROADMAP.map((option) => option.image);
 
 
     return (
         <MapLegendContainer>
             <h1>Feature Type</h1>
             <LegendItem>
-                      <LegendImage src={availableIcon} preview={false} />
-            </LegendItem>
-            <LegendItem>
-                      <LegendImage src={adoptedIcon} preview={false} />
-            </LegendItem>
-            <LegendItem>
-                      <LegendImage src={futureIcon} preview={false} />
-            </LegendItem>
+        <CheckboxContainer>
+          <Checkbox
+            checked={selectedCheckboxes.includes(availableIcon)}
+            onChange={() => handleCheckboxChange(availableIcon)}
+          />
+          <CheckboxLabel>Available</CheckboxLabel>
+        </CheckboxContainer>
+        <LegendImage src={availableIcon} preview={false} />
+      </LegendItem>
+      <LegendItem>
+        <CheckboxContainer>
+          <Checkbox
+            checked={selectedCheckboxes.includes(adoptedIcon)}
+            onChange={() => handleCheckboxChange(adoptedIcon)}
+          />
+          <CheckboxLabel>Adopted</CheckboxLabel>
+        </CheckboxContainer>
+        <LegendImage src={adoptedIcon} preview={false} />
+      </LegendItem>
+      <LegendItem>
+        <CheckboxContainer>
+          <Checkbox
+            checked={selectedCheckboxes.includes(futureIcon)}
+            onChange={() => handleCheckboxChange(futureIcon)}
+          />
+          <CheckboxLabel>Future</CheckboxLabel>
+        </CheckboxContainer>
+        <LegendImage src={futureIcon} preview={false} />
+      </LegendItem>
 
             {/* {canHide && (
         <ToggleTextButton type={'link'} onClick={toggleShowLegend}>
--- apps/green-infrastructure/frontend/src/constants.tsx
+++ apps/green-infrastructure/frontend/src/constants.tsx
@@ -1,9 +1,15 @@
 import { Loader } from "@googlemaps/js-api-loader";
 import { createMapIcon, MapIcon } from './components/mapIcon/MapIcon';
 import { allSvgMarkers } from './components/mapIcon/MapIconDesigns';
-import availableIcon from './images/siteIcons/availableIcon.svg';
-import adoptedIcon from './images/siteIcons/adoptedIcon.svg';
-import futureIcon from './images/siteIcons/futureIcon.svg';
+import availableIcon from './assets/images/siteIcons/availableIcon.svg';
+import adoptedIcon from './assets/images/siteIcons/adoptedIcon.svg';
+import futureIcon from './assets/images/siteIcons/futureIcon.svg';
+import rainIcon from './assets/images/siteIcons/rainIcon.svg';
+import swaleIcon from './assets/images/siteIcons/swaleIcon.svg';
+import bioretentionIcon from './assets/images/siteIcons/bioretentionIcon.svg';
+import porousIcon from './assets/images/siteIcons/porousIcon.svg';
+import treeIcon from './assets/images/siteIcons/treeIcon.svg';
+
 
 export const loader = new Loader({
     apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
@@ -21,15 +27,23 @@ export const BOSTON_BOUNDS = {
 
 
 export type SiteStatus = 'Available' | 'Adopted' | 'Future' ;
+export type SiteType = 'Rain Garden' | 'Swales' | 'Bioretention' | 'Porous Paving' | 'Tree Planter'
 
 
-interface SiteOption {
+interface SiteStatusOption {
     image: string;
     label: string;
     value: SiteStatus;
   }
 
-  export const SITE_OPTIONS_ROADMAP: SiteOption[] = [
+
+interface SiteTypeOption {
+  image: string;
+  label: string;
+  value: SiteType;
+}
+
+  export const SITE_STATUS_ROADMAP: SiteStatusOption[] = [
     {
       image: availableIcon,
       label: 'Available Sites',
@@ -45,6 +59,36 @@ interface SiteOption {
       label: 'Currently Unavailable Sites',
       value: 'Future',
     },
+  ];
+
+
+  export const SITE_TYPE_ROADMAP: SiteTypeOption[] = [
+    {
+      image: rainIcon,
+      label: 'Rain Garden',
+      value: 'Rain Garden',
+    },
+    {
+      image: swaleIcon,
+      label: 'Swales',
+      value: 'Swales',
+    },
+    {
+      image: bioretentionIcon,
+      label: 'Bioretention',
+      value: 'Bioretention',
+    },
+    {
+      image: porousIcon,
+      label: 'Porous Paving',
+      value: 'Porous Paving',
+    },
+    {
+      image: treeIcon,
+      label: 'Tree Planter',
+      value: 'Tree Planter',
+    },
+
   ];
 
 
