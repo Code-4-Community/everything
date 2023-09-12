a98d1dec6decb7a8e9bd92296ec9e58592b3a2ce
mahekagg <105991553+mahekagg@users.noreply.github.com>
2023-09-12 17:19:53 -0400

    created selectable buttons and attempted collapse

--- apps/green-infrastructure/frontend/src/components/map/MapLegend.tsx
+++ apps/green-infrastructure/frontend/src/components/map/MapLegend.tsx
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Button, Typography, Image, Checkbox } from 'antd';
 import { useState } from 'react';
 import { SITE_STATUS_ROADMAP, SITE_TYPE_ROADMAP } from '../../constants';
-
+import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
 
 const MapLegendContainer = styled.div`
   background: #BDBDBD;
@@ -12,16 +12,6 @@ const MapLegendContainer = styled.div`
   gap: 20px;
 `;
 
-const CheckboxContainer = styled.div`
-  display: flex;
-  align-items: right;
-  gap: 8px;
-`;
-
-const CheckboxLabel = styled.label`
-  margin-bottom: 0;
-`;
-
 const LegendItem = styled.div`
   width: 100%;
   display: flex;
@@ -29,9 +19,6 @@ const LegendItem = styled.div`
   align-items: center;
 `;
 
-const LegendImageContainer = styled.div`
-  text-align: center; 
-`;
 
 const LegendImage = styled(Image)`
   height: 20px;
@@ -40,84 +27,121 @@ const LegendImage = styled(Image)`
   display: inline-block;
 `;
 
-// const ToggleTextButton = styled(Button)`
-//   padding: 5px;
-// `;
+const StyledButton = styled.button<{ isSelected: boolean }>`
+  background-color: ${(props) => (props.isSelected ? '#e74c3c' : '#3498db')};
+  height: 38px;
+  width: 370px;
+  color: #fff;
+  border: line;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 16px;
+  align-items: center;
+`;
 
+
+const CollapsibleContainer = styled.div<{ isCollapsed: boolean }>`
+  height: ${(props) => (props.isCollapsed ? '0' : 'auto')};
+  overflow: hidden;
+  transition: height 0.3s ease;
+`;
+
+const ToggleButton = styled.button`
+  cursor: pointer;
+  font-size: 18px;
+  position: absolute;
+  bottom: 5px;
+  left: 200px;
+`;
+
+
+const CaretDownStyled = styled(CaretDownOutlined)`
+  color: #FFFFFF;
+`;
+
+const CaretUpStyled = styled(CaretUpOutlined)`
+  color: #FFFFFF;
+`;
 
 interface MapLegendProps {
-    readonly canHide?: boolean;
-    readonly icons?: string[];
-  }
+  icons: string[] | null; 
+}
 
-const MapLegend: React.FC<MapLegendProps> = ({ canHide, icons }) => {
-    // const [showLegend, setShowLegend] = useState(true);
+const MapLegend: React.FC<MapLegendProps> = ({ icons }) => {
+    const [showLegend, setShowLegend] = useState(false);
+    const [selectedFeatures, setSelectedFeatures] = useState<string[]>([]);
 
-    // const toggleShowLegend = () => {
-    //     setShowLegend((prevState) => !prevState);
-    //   };
-
-    const [selectedCheckboxes, setSelectedCheckboxes] = useState<string[]>([]);
-
-    const handleCheckboxChange = (icon: string) => {
-        setSelectedCheckboxes(prevSelected => {
-          if (prevSelected.includes(icon)) {
-            return prevSelected.filter(selected => selected !== icon);
-          } else {
-            return [...prevSelected, icon];
-          }
-        });
+    const toggleShowLegend = () => {
+        setShowLegend((prevState) => !prevState);
       };
-
 
     const [availableIcon, adoptedIcon, futureIcon] =
       icons ?? SITE_STATUS_ROADMAP.map((option) => option.image);
 
+    console.log(icons)
+
+
+    const handleFeatureClick = (icon: string) => {
+      // Check if the icon is already selected
+      const isAlreadySelected = selectedFeatures.includes(icon);
+  
+      if (isAlreadySelected) {
+        // Deselect the icon
+        setSelectedFeatures((prevSelectedFeatures) =>
+          prevSelectedFeatures.filter((selected) => selected !== icon)
+        );
+      } else {
+        // Select the icon
+        setSelectedFeatures((prevSelectedFeatures) => [...prevSelectedFeatures, icon]);
+      }
+    };
+
+    
 
     return (
-        <MapLegendContainer>
-            <h1>Feature Type</h1>
-            <LegendItem>
-        <CheckboxContainer>
-          <Checkbox
-            checked={selectedCheckboxes.includes(availableIcon)}
-            onChange={() => handleCheckboxChange(availableIcon)}
-          />
-          <CheckboxLabel>Available</CheckboxLabel>
-        </CheckboxContainer>
-        <LegendImageContainer>
-          <LegendImage src={availableIcon} alt="Available" style={{width: '20px', height: '20px', justifyContent: 'center' }} />
-        </LegendImageContainer>
-      </LegendItem>
+      <MapLegendContainer>
+        <CollapsibleContainer isCollapsed={showLegend}>
+      <h1>Feature Type</h1>
       <LegendItem>
-        <CheckboxContainer>
-          <Checkbox
-            checked={selectedCheckboxes.includes(adoptedIcon)}
-            onChange={() => handleCheckboxChange(adoptedIcon)}
-          />
-          <CheckboxLabel>Adopted</CheckboxLabel>
-        </CheckboxContainer>
-        <LegendImage src={adoptedIcon} alt="Adopted" style={{width: '20px', height: '20px', justifyContent: 'center' }} />
-      </LegendItem>
-      <LegendItem>
-        <CheckboxContainer>
-          <Checkbox
-            checked={selectedCheckboxes.includes(futureIcon)}
-            onChange={() => handleCheckboxChange(futureIcon)}
-          />
-          <CheckboxLabel>Future</CheckboxLabel>
-        </CheckboxContainer>
-        <LegendImage src={futureIcon} alt="Future" style={{width: '20px', height: '20px', justifyContent: 'center' }} />
-      </LegendItem>
-
-            {/* {canHide && (
-        <ToggleTextButton type={'link'} onClick={toggleShowLegend}>
-          {showLegend ? 'Hide Legend' : 'Show Legend'}
-        </ToggleTextButton>
-      )} */}
-        </MapLegendContainer>
-      );
-    };
+  {icons && (
+    <StyledButton
+      onClick={() => handleFeatureClick('availableIcon')} 
+      isSelected={selectedFeatures.includes('availableIcon')} 
+    >
+      <LegendImage src={availableIcon} alt="Available" style={{ width: '20px', height: '20px', justifyContent: 'center' }} />
+      Available
+    </StyledButton>
+  )}
+</LegendItem>
+<LegendItem>
+  {icons && (
+    <StyledButton
+      onClick={() => handleFeatureClick('adoptedIcon')} 
+      isSelected={selectedFeatures.includes('adoptedIcon')} 
+    >
+      <LegendImage src={adoptedIcon} alt="Adopted" style={{ width: '20px', height: '20px', justifyContent: 'center' }} />
+      Adopted
+    </StyledButton>
+  )}
+</LegendItem>
+<LegendItem>
+  {icons && (
+    <StyledButton
+      onClick={() => handleFeatureClick('futureIcon')} 
+      isSelected={selectedFeatures.includes('futureIcon')} 
+    >
+      <LegendImage src={futureIcon} alt="Future" style={{ width: '20px', height: '20px', justifyContent: 'center' }} />
+      Future
+    </StyledButton>
+  )}
+</LegendItem>
+<ToggleButton onClick={toggleShowLegend}>
+        {showLegend ? <CaretDownStyled /> : <CaretUpStyled />}
+      </ToggleButton>
+</CollapsibleContainer>
+  </MapLegendContainer>
+  );
+};
 
 
 export default MapLegend;
