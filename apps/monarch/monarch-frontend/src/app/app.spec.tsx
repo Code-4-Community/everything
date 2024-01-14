b0086e854e70993a2e5899673bad6bcb2c5c2878
Harrison Kim <kim.harr@northeastern.edu>
2024-01-14 16:41:11 -0500

    fix: copied GI test file

--- apps/monarch/monarch-frontend/src/app/app.spec.tsx
+++ apps/monarch/monarch-frontend/src/app/app.spec.tsx
@@ -1,27 +1,23 @@
-// import { render } from '@testing-library/react';
-
-// import { BrowserRouter } from 'react-router-dom';
-// import { describe } from 'node:test';
-
-// describe('App', () => {
-//   it('should pass', () => {
-//     expect(1 + 1).toBe(2);
-//   });
-//   /* it('should render successfully', () => {
-//    *   const { baseElement } = render(
-//    *     <BrowserRouter>
-//    *       <App />
-//    *     </BrowserRouter>
-//    *   );
-//    *   expect(baseElement).toBeTruthy();
-//    * });
-
-//    * it('should have a greeting as the title', () => {
-//    *   const { getByText } = render(
-//    *     <BrowserRouter>
-//    *       <App />
-//    *     </BrowserRouter>
-//    *   );
-//    *   expect(getByText(/Welcome frontend/gi)).toBeTruthy();
-//    * }); */
-// });
+describe('App', () => {
+    it('should pass', () => {
+      expect(1 + 1).toBe(2);
+    });
+    /* it('should render successfully', () => {
+     *   const { baseElement } = render(
+     *     <BrowserRouter>
+     *       <App />
+     *     </BrowserRouter>
+     *   );
+     *   expect(baseElement).toBeTruthy();
+     * });
+  
+     * it('should have a greeting as the title', () => {
+     *   const { getByText } = render(
+     *     <BrowserRouter>
+     *       <App />
+     *     </BrowserRouter>
+     *   );
+     *   expect(getByText(/Welcome frontend/gi)).toBeTruthy();
+     * }); */
+  });
+  
